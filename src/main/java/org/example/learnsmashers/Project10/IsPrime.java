38922397a73befce6729540e28055b918bb1c2d2
KartikSGunaga <kartik.s@learner.manipal.edu>
2024-01-13 10:55:59 +0530

    Updated Prime checker Project

--- src/main/java/org/example/learnsmashers/Project10/IsPrime.java
+++ src/main/java/org/example/learnsmashers/Project10/IsPrime.java
@@ -1,2 +1,28 @@
-package org.example.learnsmashers.Project10;public class IsPrime {
+package org.example.learnsmashers.Project10;
+
+public class IsPrime {
+    private int num;
+
+    public IsPrime(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public boolean check() {
+        if (this.num == 2 || this.num == 3)
+            return true;
+
+        for (int i = this.num - 1; i > 3; i--) {
+            if (this.num % i == 0)
+                return false;
+        }
+        return true;
+    }
 }
--- src/main/java/org/example/learnsmashers/Project10/Main.java
+++ src/main/java/org/example/learnsmashers/Project10/Main.java
@@ -1,2 +1,32 @@
-package org.example.learnsmashers.Project10;public class Main {
+package org.example.learnsmashers.Project10;
+
+import org.controlsfx.control.tableview2.filter.filtereditor.SouthFilter;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to prime number checker!");
+        String repeat = null;
+
+        while (true) {
+            System.out.println("\nEnter the number: ");
+            int num = scanner.nextInt();
+
+            IsPrime isPrime = new IsPrime(num);
+            if (isPrime.check()) {
+                System.out.println(num + " is a prime number.");
+            } else {
+                System.out.println(num + " is not a prime number.");
+            }
+            System.out.println("\nDo you wish to check for another number?(yes/no)");
+            repeat = scanner.next();
+
+            if (repeat.equals("no"))
+                break;
+        }
+
+        System.out.println("Thank you!");
+    }
 }

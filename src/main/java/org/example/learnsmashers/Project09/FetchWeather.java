d4341aff7e20b338a1e874e0705ee29da6213af1
KartikSGunaga <kartik.s@learner.manipal.edu>
2024-01-13 10:55:39 +0530

    Updated Weather Forecast Project

--- src/main/java/org/example/learnsmashers/Project09/FetchWeather.java
+++ src/main/java/org/example/learnsmashers/Project09/FetchWeather.java
@@ -1,2 +1,71 @@
-package org.example.learnsmashers.Project09;public class FetchWeather {
+package org.example.learnsmashers.Project09;
+
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class FetchWeather {
+    private String city, apiKey;
+
+    public FetchWeather(String city, String apiKey) {
+        this.apiKey = apiKey;
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public void currentWeather() throws Exception {
+        String endPoint = "http://api.weatherapi.com/v1/current.json?key="
+                + this.apiKey + "&q=" + this.city + "&aqi=yes";
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        URI uri = new URI(endPoint);
+
+        HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            System.out.println("\nRequest successful!");
+            JSONObject json = new JSONObject(response.body());
+            JSONObject location = json.getJSONObject("location");
+            JSONObject current = json.getJSONObject("current");
+
+            String lastUpdated = current.getString("last_updated");
+            String place = location.getString("name") + ", "
+                    + location.getString("region")+ ", "
+                    + location.getString("country");
+            String timeZone = location.getString( "tz_id");
+            String localTime = location.getString("localtime");
+            float tempCelsius = current.getFloat("temp_c");
+            String feelsLike = current.getJSONObject("condition").getString("text");
+
+            System.out.println("\nFetched data for " + place);
+            System.out.println("Fetched at: " + lastUpdated + " (TimeZone: " + timeZone + ")");
+            System.out.println("Local time in " + location.getString("name") + ": " + localTime + " (TimeZone: " + timeZone + ")");
+            System.out.println("Current temperature: " + tempCelsius);
+            System.out.println("Feels like " + feelsLike);
+
+        } else {
+            System.out.println("Oops!");
+            throw new RuntimeException("Error calling API response: " + response.body());
+        }
+    }
 }
--- src/main/java/org/example/learnsmashers/Project09/Main.java
+++ src/main/java/org/example/learnsmashers/Project09/Main.java
@@ -1,2 +1,28 @@
-package org.example.learnsmashers.Project09;public class Main {
+package org.example.learnsmashers.Project09;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to weather forecast!");
+
+        String apiKey = "5abb6102230d411889270612241101", city = null, repeat = null;
+
+        while(true){
+            System.out.println("\nEnter the city name: ");
+            city = scanner.next();
+            System.out.println("Please wait...");
+
+            FetchWeather fetch = new FetchWeather(city, apiKey);
+            fetch.currentWeather();
+
+            System.out.println("\nDo you want to search again?(y/n): ");
+            repeat = scanner.next().toLowerCase();
+
+            if (repeat.equals("n"))
+                break;
+        }
+        System.out.println("\nThank you!");
+    }
 }
